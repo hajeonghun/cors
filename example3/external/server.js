29337a3688c4965188ecfaec18df34c4a27ce5e6
하정훈 <hajh1994@gmail.com>
2025-01-24 16:19:02 +0900

    chore: add example3

---
+++ example3/external/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+// CORS 설정 (특정 출처만 허용)
+app.use(cors());
+
+// GET 요청
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'Hello Simple Request!', data: [1, 2, 3, 4, 5] });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port :${PORT}`);
+});
