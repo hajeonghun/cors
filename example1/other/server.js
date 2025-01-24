40d1d1c3f5c5f785642ee58364beb471f448b141
하정훈 <hajh1994@gmail.com>
2025-01-24 11:38:42 +0900

    chore: update example1

--- example1/other/server.js
+++ example1/other/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const PORT = 3000;
 
 // CORS 설정 (모든 도메인 허용)
 app.use(cors());
@@ -9,4 +10,8 @@ app.use(cors());
 // GET 요청
 app.get('/api/data', (req, res) => {
     res.json({ message: 'Hello Simple Request!', data: [1, 2, 3, 4, 5] });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port :${PORT}`);
 });
